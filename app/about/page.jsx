33fe246d0dc8658933422d7a0d1dffc9e43ee0dd
Yeray Guachetá Carreño <yeye20041@gmail.com>
2024-04-29 18:10:26 -0500

    Starting About us section

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,26 @@
 export default function About() {
   return (
     <main>
-      About
+      <h1>Who we are</h1>
+      <section>
+        <div>
+          <p>Image1</p>
+          <p>Image2</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <hr />
+          <hr />
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <div>
+            <button>Services</button>
+            <button>See our work</button>
+          </div>
+        </div>
+      </section>
+      <section>
+        iamge gallery
+      </section>
     </main>
   )
 }
